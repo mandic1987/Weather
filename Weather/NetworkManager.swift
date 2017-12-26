64f102a588941611b55c254cf103b29f44346b1f
milos.mandic@openratio.com <milos.mandic@openratio.com>
2017-12-26 15:20:09 +0100

    Create Path url and urlRequest

--- Weather/NetworkManager.swift
+++ Weather/NetworkManager.swift
@@ -1,5 +1,5 @@
 //
-//  WeatherManager.swift
+//  NetworkManager.swift
 //  Weather
 //
 //  Created by iOS on 12/26/17.
@@ -9,5 +9,69 @@
 import Foundation
 
 final class NetworkManager {
-    
+    enum  Path {
+        static let host = "http://api.openweathermap.org/data/2.5/forecast/daily"
+        static let count = "7"
+        static let apiKey = "59f3ee883bd98efaba93fe0f4a8cf516"
+        
+        case city(n: String)
+        case id(id: Int)
+        case coordinates(lat: Double, lon: Double)
+        case zipCode(zip: Int)
+        
+        private var url: URL {
+            var s = ""
+            
+            switch self {
+            case .city(let n):
+                s = "\(Path.host)?q=\(n)&cnt=\(Path.count)&appid=\(Path.apiKey)"
+            case .id(let id):
+                s = "\(Path.host)?id=\(id)&cnt=\(Path.count)&appid=\(Path.apiKey)"
+            case .coordinates(let lat, let lon):
+                s = "\(Path.host)?lat=\(lat)&lon=\(lon)&cnt=\(Path.count)&appid=\(Path.apiKey)"
+            case .zipCode(let zip):
+                s = "\(Path.host)?zip=\(zip)&appid=\(Path.apiKey)"
+            }
+         
+            return URL(string: s)!
+        }
+        
+        private var method: String {
+            switch self {
+            case .city:
+                return "GET"
+            case .id:
+                return "GET"
+            case .coordinates:
+                return "GET"
+            case .zipCode:
+                return "GET"
+            }
+        }
+        
+        var urlRequest: URLRequest {
+            var urlRequest = URLRequest(url: url)
+            urlRequest.httpMethod = method
+            return urlRequest
+        }
+    }
+
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
