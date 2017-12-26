da2c1ffbc9d42de94ece8743775803bf3004ffca
milos.mandic@openratio.com <milos.mandic@openratio.com>
2017-12-26 13:57:26 +0100

    Create empty skeleton for project and initial setup of project

---
+++ Weather/NetworkManager.swift
@@ -0,0 +1,13 @@
+//
+//  WeatherManager.swift
+//  Weather
+//
+//  Created by iOS on 12/26/17.
+//  Copyright © 2017 WoodCode. All rights reserved.
+//
+
+import Foundation
+
+final class NetworkManager {
+    
+}
