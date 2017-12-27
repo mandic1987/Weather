a91c19ee06b1e0f05db18acfdcd6b6bfc57f4971
milos.mandic@openratio.com <milos.mandic@openratio.com>
2017-12-27 14:42:49 +0100

    Create Forecast class for weather data

---
+++ DataModel/Forecast.swift
@@ -0,0 +1,13 @@
+//
+//  Forecast.swift
+//  Weather
+//
+//  Created by iOS on 12/27/17.
+//  Copyright © 2017 WoodCode. All rights reserved.
+//
+
+import Foundation
+
+final class Forecast {
+    
+}
