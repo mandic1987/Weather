d2f7d95b2c076e5cb154a69d9b2beda086103f6f
milos.mandic@openratio.com <milos.mandic@openratio.com>
2018-01-10 11:29:14 +0100

    Added SearchType enum

---
+++ Network/SearchType.swift
@@ -0,0 +1,18 @@
+//
+//  SearchType.swift
+//  Weather
+//
+//  Created by iOS on 1/10/18.
+//  Copyright © 2018 WoodCode. All rights reserved.
+//
+
+import Foundation
+
+extension NetworkManager {
+    enum SearchType {
+        case name
+        case id
+        case coordinates
+        case zipCode
+    }
+}
