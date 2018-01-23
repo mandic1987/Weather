e8317fb2f0652077416a37e5524b8ab44ab9bfcb
milos.mandic@openratio.com <milos.mandic@openratio.com>
2018-01-23 14:50:15 +0100

    Implemented logic for Search and connect UI to code

--- Weather/WeatherController.swift
+++ Weather/WeatherController.swift
@@ -31,3 +31,10 @@ class WeatherController: UIViewController {
         }
     }
 }
+
+extension WeatherController {
+    @IBAction func textFieldChanged(_ sender: UITextField) {
+        guard let s = sender.text else { return }
+        search(for: s)
+    }
+}
